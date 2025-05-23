62270cc67b56d2652b05a50065e1ee458f59d622
vitaglianog <vitaglianog@gmail.com>
2025-05-23 10:20:16 -0400

    exec

--- exec_script.sh
+++ exec_script.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-# for workload in "archeology" "astronomy" "legal" "biomedical"; do
-for workload in "biomedical"; do
-
-for sut_name in "BaselineLLMSystemDeepseekR1"; do
-#  for config in "Naive" "OneShot" "FewShot"; do
- for config in "Naive"; do
-    echo "Running workload: $workload with SUT: $sut_name$config"
-    python evaluate.py --sut $sut_name$config --workload $workload.json --dataset_name $workload
+for workload in "archeology" "astronomy" "biomedical" "legal" "environment" "wildfire"; do
+for sut_name in "BaselineLLMSystemDeepseekR1" "BaselineLLMSystemGPTo3" "BaselineLLMSystemGPT4o" "BaselineLLMSystemQwen2_5Coder" BaselineLLMSystemLlama3_3Instruct;  do
+for config in "Naive" "OneShot" "FewShot"; do
+    # echo "Running workload: $workload with SUT: $sut_name$config"
+    screen -d -m python evaluate.py --sut $sut_name$config --workload $workload.json --dataset_name $workload
 done
 done
 done
